9b290153d2f81821d8151f0d71dcbe6a8cdb7a47
Varun Nath <nathvarun@hotmail.com>
2019-07-13 20:52:38 +0530

    Completed Project.

--- App.js
+++ App.js
@@ -1,53 +1,62 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  Image,
-  Animated
-} from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import Animated from 'react-native-reanimated';
+
+// core Snoopy
+import Snoopy from 'rn-snoopy';
+
+// some Snoopy goodies we're going to use
+import filter from 'rn-snoopy/stream/filter';
+
+import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
+
+//If you are using React 0.48 or below, then you should import:
+//import EventEmitter from 'react-native/Libraries/EventEmitter/EventEmitter';
+
+const emitter = new EventEmitter();
+
+const events = Snoopy.stream(emitter);
+filter({ method: 'updateView' }, true)(events).subscribe();
 
 HEADER_MAX_HEIGHT = 120;
 HEADER_MIN_HEIGHT = 70;
 PROFILE_IMAGE_MAX_HEIGHT = 80;
 PROFILE_IMAGE_MIN_HEIGHT = 40;
 
+const { Value, Extrapolate } = Animated;
+
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      scrollY: new Animated.Value(0)
-    };
-  }
-  render() {
-    const headerHeight = this.state.scrollY.interpolate({
+    this.scrollY = new Value(0);
+
+    this.headerHeight = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
-      extrapolate: 'clamp'
+      extrapolate: Extrapolate.CLAMP
     });
-    const profileImageHeight = this.state.scrollY.interpolate({
+    this.profileImageHeight = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [PROFILE_IMAGE_MAX_HEIGHT, PROFILE_IMAGE_MIN_HEIGHT],
-      extrapolate: 'clamp'
+      extrapolate: Extrapolate.CLAMP
     });
 
-    const profileImageMarginTop = this.state.scrollY.interpolate({
+    this.profileImageMarginTop = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
       outputRange: [
         HEADER_MAX_HEIGHT - PROFILE_IMAGE_MAX_HEIGHT / 2,
         HEADER_MAX_HEIGHT + 5
       ],
-      extrapolate: 'clamp'
+      extrapolate: Extrapolate.CLAMP
     });
-    const headerZindex = this.state.scrollY.interpolate({
+    this.headerZindex = this.scrollY.interpolate({
       inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT, 120],
       outputRange: [0, 0, 1000],
-      extrapolate: 'clamp'
+      extrapolate: Extrapolate.CLAMP
     });
 
-    const headerTitleBottom = this.state.scrollY.interpolate({
+    this.headerTitleBottom = this.scrollY.interpolate({
       inputRange: [
         0,
         HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT,
@@ -59,11 +68,20 @@ class App extends Component {
           26
       ],
       outputRange: [-20, -20, -20, 0],
-      extrapolate: 'clamp'
+      extrapolate: Extrapolate.CLAMP
     });
+  }
 
+  componentDidMount() {
+    // setInterval(() => {
+    //   for (let i = 0; i < 10000; i++) {
+    //     console.log(i);
+    //   }
+    // }, 200);
+  }
+  render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, shadowColor: 'white' }}>
         <Animated.View
           style={{
             position: 'absolute',
@@ -71,14 +89,14 @@ class App extends Component {
             left: 0,
             right: 0,
             backgroundColor: 'lightskyblue',
-            height: headerHeight,
-            zIndex: headerZindex,
-            elevation: headerZindex, //required for android
+            height: this.headerHeight,
+            zIndex: this.headerZindex,
+            // elevation: this.headerZindex, //required for android
             alignItems: 'center'
           }}
         >
           <Animated.View
-            style={{ position: 'absolute', bottom: headerTitleBottom }}
+            style={{ position: 'absolute', bottom: this.headerTitleBottom }}
           >
             <Text style={{ color: 'white', fontSize: 14, fontWeight: 'bold' }}>
               Varun Nath
@@ -86,27 +104,27 @@ class App extends Component {
           </Animated.View>
         </Animated.View>
 
-        <ScrollView
+        <Animated.ScrollView
           style={{ flex: 1 }}
           scrollEventThrottle={16}
           onScroll={Animated.event([
-            { nativeEvent: { contentOffset: { y: this.state.scrollY } } }
+            { nativeEvent: { contentOffset: { y: this.scrollY } } }
           ])}
         >
           <Animated.View
             style={{
-              height: profileImageHeight,
-              width: profileImageHeight,
+              height: this.profileImageHeight,
+              width: this.profileImageHeight,
               borderRadius: PROFILE_IMAGE_MAX_HEIGHT / 2,
               borderColor: 'white',
               borderWidth: 3,
               overflow: 'hidden',
-              marginTop: profileImageMarginTop,
+              marginTop: this.profileImageMarginTop,
               marginLeft: 10
             }}
           >
             <Image
-              source={require('./assets/me.jpg')}
+              source={require('./assets/me.png')}
               style={{ flex: 1, width: null, height: null }}
             />
           </Animated.View>
@@ -117,7 +135,7 @@ class App extends Component {
           </View>
 
           <View style={{ height: 1000 }} />
-        </ScrollView>
+        </Animated.ScrollView>
       </View>
     );
   }
@@ -127,6 +145,7 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center'
   }
